b36b2e0eee86c1fafb852fae08dc4093be15b0cd
Gareth George <garethgeorge97@gmail.com>
2020-12-28 08:31:08 +0000

    fixed player freezes

--- src/lib/stream_cache.ts
+++ src/lib/stream_cache.ts
@@ -3,35 +3,53 @@ import stream from "stream";
 export default class StreamCache {
   private stream: stream.Stream;
   private buffers: Buffer[];
+  private destinations: stream.Writable[];
   private ended: boolean;
-  constructor(stream: stream.Readable) {
+  constructor(stream: stream.Readable, flushIntervalMs: number = 100) {
     this.stream = stream;
     this.buffers = [];
+    this.destinations = [];
     this.ended = false;
-    stream.on("data", (data) => {
+
+    /*
+      interval flushing logic, this reduces the number of chunks 
+      generated. This reduces overhead and improves performance/reliability
+    */
+    let buffer = [];
+    const flush = () => {
+      if (buffer.length === 0) return;
+      const data = Buffer.concat(buffer);
+      for (const dest of this.destinations) {
+        dest.write(data);
+      }
       this.buffers.push(data);
+      buffer = [];
+    };
+
+    stream.on("data", (data) => {
+      buffer.push(data);
     });
+
+    const flushInterval = setInterval(flush, flushIntervalMs);
+
     stream.on("end", () => {
       this.ended = true;
+      clearInterval(flushInterval);
+      flush();
+      for (const dest of this.destinations) {
+        dest.end();
+      }
     });
   }
 
   pipe(stream: stream.Writable) {
     // catch up by writing the existing buffers, they should hopefully be small enough to bufer in memory
     for (const buffer of this.buffers) {
-      stream.write(buffer, "binary");
+      stream.write(buffer);
     }
     if (this.ended) {
       stream.end();
-    } else {
-      this.stream.on("data", data => {
-        stream.write(data, "binary");
-      });
-      this.stream.on("end", () => {
-        stream.end();
-      });
-    }
-      
+    } else this.destinations.push(stream);
   }
 
   data(): Buffer {
